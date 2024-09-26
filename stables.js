0aa2324e7dfdb88f4942f814f64ded5cc97c8863
S-Volta-tori <domo.5ator1.sama@gmail.com>
2024-09-26 16:14:42 -0700

    adding stables

---
+++ stables.js
@@ -0,0 +1,33 @@
+let horsename = "THE GUY"
+console.log (horsename);
+let horseage = 300;
+console.log (horseage);
+
+let isHorseInside = true;
+console.log (isHorseInside);
+
+const STABLE_MONTHLY_FEE = 200;
+console.log (STABLE_MONTHLY_FEE);
+console.log (STABLE_MONTHLY_FEE + STABLE_MONTHLY_FEE);
+console.log (2*3);
+console.log (horseage * horseage);
+
+let horseIntroduction ="Is the name of my horse";
+let rentIntroduction ="it costs"+ STABLE_MONTHLY_FEE + horsename;
+
+console.log(horseIntroduction);
+console.log(rentIntroduction);
+
+let stableinstruction="Welcome to Subterrarean.--Satori Komeji"
+console.log(stableinstruction)
+
+///////LAB 4 Code//////
+let horsenickname ="THE GUY"
+console.log(horsenickname)
+
+let horseinstruction =" is a carrot killer"
+
+console.log(horsenickname + horseinstruction)
+
+let horseMessage = "three month stay at the stable with a 10% discount " + (STABLE_MONTHLY_FEE*3 -60)
+console.log(horseMessage)
